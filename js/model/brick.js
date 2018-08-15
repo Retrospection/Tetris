dd66c230b61dfddd251330da5fa72d4f11092898
Retros.Yu <yuruosheng@jd.com>
2018-08-15 10:09:59 +0800

    brickI move left finished

--- js/model/brick.js
+++ js/model/brick.js
@@ -8,33 +8,65 @@ function BrickI (position, grid) {
 
 // 砖块移动
 BrickI.prototype.move = function (direction) {
-
     _moveBrick(this, direction)
-    
-   
 }
 
 function _moveBrick (brick, direction) {
 
     let originPosition = []
-
+    
     // 将方块当前位置置 0
-    for (let i = 0; i < brick.coordinate.length; ++i) {
-        let x = brick.coordinate[i][0]
-        let y = brick.coordinate[i][1]
+    brick.coordinate.forEach(function (coordinate) {
+        let x = coordinate[0]
+        let y = coordinate[1]
         originPosition.push([x, y])
         brick.grid.data[x][y] = 0
-    }
+    })
     
     let result
+
     // 将方块坐标根据方向进行变更
     if (direction === 'left') {
-        // 检查是否有格子的值为2
 
-        // 如果没有，则完成移动。 如果有则将方块位置回退。
+        // 获得砖块的新位置
+        let newPosition = originPosition.map(function (coordinate) {
+            if (coordinate[0] - 1 < 0) {
+                return coordinate
+            }
+            return [coordinate[0] - 1, coordinate[1]]
+        })
+        
+        // 计算应用新位置的时候，grid的值
+        newPosition.forEach(function (coordinate) {
+            let x = coordinate[0]
+            let y = coordinate[1]
+            brick.grid[x][y] += 1
+        })
 
-        // 返回值 true 为完成移动，false 为已经不可朝该方向移动
-
+        // 检查grid中是否出现大于1的格子
+        let valueGreaterThanTwo
+        for (let row = 0; row < brick.grid.length; ++row) {
+            valueGreaterThanTwo = brick.grid[row].filter(function (value) {
+                return value > 1
+            })
+        }
+        
+        if (valueGreaterThanTwo.length > 0) {
+            // rollback
+            newPosition.forEach(function (coordinate) {
+                let x = coordinate[0]
+                let y = coordinate[1]
+                brick.grid[x][y] -= 1
+            })
+            orientation.forEach(function (coordinate) {
+                let x = coordinate[0]
+                let y = coordinate[1]
+                brick.grid[x][y] += 1
+            })
+            result = false
+        }
+        result = true
+        
     } else if (direction === 'right') {
 
     } else if (direction === 'down') {
@@ -42,6 +74,5 @@ function _moveBrick (brick, direction) {
     } else if (direction === 'bottom') {
 
     }
-
     return result
 }
